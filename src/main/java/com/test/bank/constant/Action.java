a528aa3aa5970a5e415d2562d2bcaa2233c77d91
Jeremy Chen <chenpighead@gmail.com>
2019-02-07 20:08:56 +0800

    Add user wallet transaction: transfer (should add transaction log)
    Fix TODO implement transfer
    Add Action constant to represent and store transaction log

---
+++ src/main/java/com/test/bank/constant/Action.java
@@ -0,0 +1,6 @@
+package com.test.bank.constant;
+
+public class Action {
+    public static final String TRANSFER = "1";
+    public static final String CREDIT_AND_DEBIT = "2";
+}
--- src/main/java/com/test/bank/service/TransactionService.java
+++ src/main/java/com/test/bank/service/TransactionService.java
@@ -1,15 +1,25 @@
 package com.test.bank.service;
 
+import com.test.bank.constant.Action;
 import com.test.bank.initializer.DataSourceInitializer;
 import com.test.bank.model.transaction.TransactionVo;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
 import org.jooq.impl.DefaultConfiguration;
+import org.jooq.types.UInteger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import static com.test.bank.db.tables.Transaction.TRANSACTION;
+import static com.test.bank.db.tables.User.USER;
+
 @Singleton
 public class TransactionService {
 
+    Logger log = LoggerFactory.getLogger(TransactionService.class);
     DefaultConfiguration jooqConfiguration;
 
     @Inject
@@ -17,8 +27,70 @@ public class TransactionService {
         this.jooqConfiguration = dataSourceInitializer.getJooqConfiguration();
     }
 
+    private boolean validateUserId(int userId) {
+        DSLContext dsl = DSL.using(this.jooqConfiguration);
+        return dsl.fetchExists(dsl
+                .selectOne()
+                .from(USER)
+                .where(USER.ID.eq((UInteger.valueOf(userId)))));
+    }
+
     public void transfer(int fromUserId, int toUserId, int amount, int adminId) {
         // TODO implement transfer
+        if (!validateUserId(fromUserId)) {
+            log.debug("Invalid fromUserId: " + fromUserId);
+            return;
+        }
+        if (!validateUserId(toUserId)) {
+            log.debug("Invalid toUserId: " + toUserId);
+            return;
+        }
+        DSLContext dsl = DSL.using(this.jooqConfiguration);
+        int fromUserWallet = dsl
+                .select()
+                .from(USER)
+                .where(USER.ID.eq(UInteger.valueOf(fromUserId)))
+                .fetchOne()
+                .getValue(USER.WALLET);
+        if (fromUserWallet < amount) {
+            log.debug("Not enough, transfer " + amount + " with wallet: " + fromUserWallet);
+            return;
+        }
+        int toUserWallet = dsl
+                .select()
+                .from(USER)
+                .where(USER.ID.eq(UInteger.valueOf(toUserId)))
+                .fetchOne()
+                .getValue(USER.WALLET);
+        // Use jOOQ transaction API to help with transaction / rollback management
+        // ref: https://www.jooq.org/doc/3.11/manual/sql-execution/transaction-management/
+        try {
+            dsl.transaction(configuration -> {
+                DSL.using(configuration)
+                        .insertInto(USER, USER.ID, USER.WALLET)
+                        .values(UInteger.valueOf(fromUserId), fromUserWallet - amount)
+                        .onDuplicateKeyUpdate()
+                        .set(USER.WALLET, fromUserWallet - amount)
+                        .execute();
+                DSL.using(configuration)
+                        .insertInto(USER, USER.ID, USER.WALLET)
+                        .values(UInteger.valueOf(toUserId), toUserWallet + amount)
+                        .onDuplicateKeyUpdate()
+                        .set(USER.WALLET, toUserWallet + amount)
+                        .execute();
+            });
+        } catch (RuntimeException e) {
+            log.debug("Transaction 'transfer' failed, rolls back the transaction");
+            throw new RuntimeException(e.getMessage(), e);
+        }
+        log.debug("Transaction 'transfer' successful");
+        dsl.insertInto(TRANSACTION,
+                TRANSACTION.FROMUSERID, TRANSACTION.TOUSERID, TRANSACTION.ACTION, TRANSACTION.ADMINID)
+                .values(UInteger.valueOf(fromUserId),
+                        UInteger.valueOf(toUserId),
+                        Byte.valueOf(Action.TRANSFER),
+                        UInteger.valueOf(adminId))
+                .execute();
     }
 
     public TransactionVo getTransactionLog(int userId) {
